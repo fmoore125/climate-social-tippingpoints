110b58996914f34851f75173a9b387f5f7ceae1e
fmoore125 <fmoore125@gmail.com>
2021-04-20 20:32:56 -0700

    climate perception tipping points

--- src/model_analysis/tippingelements_analysis.R
+++ src/model_analysis/tippingelements_analysis.R
@@ -61,7 +61,7 @@ params=expand.grid(lbd_sweep,fracsupp_sweep,sq_sweep)
 ems_output=numeric()
 for(i in 1:dim(params)[1]){
   m=model(lbd_param=params[i,1],frac_opp_0=0.5-params[i,2],pol_response=params[i,3])
-  ems_output[i]=sum(m$emissions[6:86])
+  ems_output[i]=sum(m$totalemissions[6:86])
 }
 
 params$emissions=ems_output
@@ -75,3 +75,49 @@ a=a+scale_fill_gradient(low="yellow",high="turquoise3")
 x11()
 a
 
+#3. Perception of Climate Change
+
+frac_opp_01=0.26 #doubtful and dismissive (global warming 6 americas)
+frac_neut_01=0.33 #cautious and disengaged (global warming 6 americas)
+policyopinionfeedback_01=0
+
+biassedass_sweep=seq(0,0.9,by=0.05)
+perception_sweep=seq(0,0.25,by=0.025 )
+baselines=c(0,1)
+nethom_sweep=c(0.35,0.95)
+
+params=expand.grid(biassedass_sweep,perception_sweep,baselines,nethom_sweep)
+
+dist_output_pro=numeric()
+dist_output_con=numeric()
+year=2050
+reps=500
+for(i in 1:dim(params)[1]){
+  print(i)
+  temp1=numeric(length=reps);temp2=numeric(length=reps)
+  if(params[i,3]==0){
+    m=model(biassedassimilation=params[i,1],evidenceeffect=params[i,2],shiftingbaselines=params[i,3],homophily_param=params[i,4])
+    dist_output_con[i]=m$distributions[which(m$year==year),1];dist_output_pro[i]=m$distributions[which(m$year==year),3]
+    next
+  }
+  if(params[i,3]>0){ #shifting baselines have a random component so average over many model runs
+    for(j in 1:reps){
+      m=model(biassedassimilation=params[i,1],evidenceeffect=params[i,2],shiftingbaselines=params[i,3],homophily_param=params[i,4])
+      temp1[j]=m$distributions[which(m$year==year),3]
+      temp2[j]=m$distributions[which(m$year==year),1]
+    }
+    dist_output_pro[i]=mean(temp1);dist_output_con[i]=mean(temp2)
+  }
+}
+
+params$opposed=dist_output_con
+colnames(params)=c("biassed_assimilation","evidenceeffect","shiftingbaseline","networkhomophily","opposers2050")
+params$shiftingbaseline=as.factor(params$shiftingbaseline);params$shiftingbaseline=fct_recode(params$shiftingbaseline,"Shifting Baseline"="1","Fixed Baseline"="0")
+params$networkhomophily=as.factor(params$networkhomophily);params$networkhomophily=fct_recode(params$networkhomophily,"Mixed Population"="0.35","Unmixed Population"="0.95")
+
+a=ggplot(params,aes(x=biassed_assimilation,y=evidenceeffect,fill=opposers2050*100))+geom_tile()
+a=a+facet_grid(networkhomophily~shiftingbaseline)+labs(x="Biassed Assmiliation",y="Effect of Perceived Weather on Opinion",fill="Policy Opposers\n2050 (%)")
+a=a+theme_bw()+theme(strip.background =element_rect(fill="white"),legend.title.align = 0.5,legend.box.just = "center",strip.text=element_text(face="bold",size=12),text=element_text(size=14))
+a=a+scale_fill_gradient(low="green",high="darkorchid")
+x11()
+a
